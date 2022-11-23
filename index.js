52abea0a466b3a6f4d488de00cf07d4ea0ef79e3
MandieBot <amandabotinelly@gmail.com>
2022-11-23 15:57:40 -0700

    Writing out logic for questions arrays

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const questions = [
+const employees = [];
+
+const managerQuestions = [
   {
     type: "input",
     name: "name",
@@ -19,15 +21,12 @@ const questions = [
   },
   {
     type: "input",
-    name: "office",
+    name: "officeNumber",
     message: "What is the manager's office number?",
   },
-  {
-    type: "list",
-    name: "role",
-    message: "Which role would you like to add to your team? (Use arrow keys to select an option)",
-    choices: ["Engineer", "Intern", "I am done adding team members"],
-  },
+];
+
+const engQuestions = [
   {
     type: "input",
     name: "name",
@@ -48,12 +47,9 @@ const questions = [
     name: "github",
     message: "What is your Engineer's GitHub username?",
   },
-  {
-    type: "list",
-    name: "role",
-    message: "Which role would you like to add to your team? (Use arrow keys to select an option)",
-    choices: ["Engineer", "Intern", "I am done adding team members"],
-  },
+];
+
+const internQuestions = [
   {
     type: "input",
     name: "name",
@@ -74,6 +70,9 @@ const questions = [
     name: "education",
     message: "Where is your Intern completing their education?",
   },
+];
+
+const roleQuestion = [
   {
     type: "list",
     name: "role",
@@ -81,3 +80,39 @@ const questions = [
     choices: ["Engineer", "Intern", "I am done adding team members"],
   },
 ];
+
+function roleSurvey() {
+  inquirer.prompt(roleQuestion).then((response) => {
+    if (response.role === "Engineer") {
+      engSurvey();
+    }
+    if (response.role === "Intern") {
+      internSurvey();
+    } else {
+      console.log(employees); //done adding members
+    }
+  });
+}
+
+function engSurvey() {
+  inquirer.prompt(engQuestions).then((response) => {
+    employees.push(response);
+    console.log(employees);
+    roleSurvey();
+  });
+}
+
+function internSurvey() {
+  inquirer.prompt(internQuestions).then((response) => {
+    employees.push(response);
+    roleSurvey();
+  });
+}
+
+function initialize() {
+  inquirer.prompt(managerQuestions).then((response) => {
+    employees.push(response);
+    roleSurvey();
+  });
+}
+initialize();
